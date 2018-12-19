1bd503d66adb65c07d0adf54501a64ab80035e86
ralracish <carlarblackman@gmail.com>
2018-12-19 13:00:16 -0500

    1st revision

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+<html>
+
+<script>
+
+
+document.onkeyup = function(event) {
+    var keyPressed = event.key.toLowerCase ();
+    if(keyPressed) === "t" || keyPressed === "f") {  
+    console.log("key pressed: " + event.key);
+    }
+};
+
+reWrite stats();
+
+</script>
