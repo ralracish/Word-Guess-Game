172e1df69564b8f67f677fb3cc3bf93c31e5a2cb
ralracish <carlarblackman@gmail.com>
2018-12-20 15:56:10 -0500

    some work towards a game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,54 @@
+//Javascript Document
 
-<html>
+var coffeeWords = ["coffee", "macchiato", "americano", "mocha", "beans", "cream", "sugar", "cappucino", "latte", "barista", "crema", "espresso", "filter", "redeye"]
 
-<script>
+var choice = Math.floor(Math.random()*coffeeWords.length;
+var answer = coffeeWords[choice];
+
+var answerArray = [];
+ for (var i = 0; i < word.length; i++) 
+ answerArray[i] = "_";
+}
+
+var remainingLetters = word.length;
+while (remainingLetters > 0) {
+    (answerArray.join(" "));
+    && var guess.toLowerCase=10-i;
+    alert("Guess the word! Type your letter. You have (10-i) guesses!")
+}
+
+var letters = []
+for(var i = 0; i < 3; i++)
+    letters.push(prompt("Guess the word! Type your letter!"));
+
 
 
 document.onkeyup = function(event) {
     var keyPressed = event.key.toLowerCase ();
-    if(keyPressed) === "t" || keyPressed === "f") {  
+    if(keyPressed) === "t" || keyPressed === "f") 
+    {
+        
+for (i=0;i<10;i++){
+    guess = prompt("Guess the word! Type your letter. You have (10-i) guesses.");
+    if coffeeWords === guess){
+        alert("You guessed the word!");
+        break;
+    }else { i=0
+        guess=prompt("You lost.");
+    }
+    }
+}
+for (var j = 0; j < word.length; j++) {
+     if (word[j] === guess) {
+     answerArray[j] = guess;
+     remainingLetters--;
+     }
+
     console.log("key pressed: " + event.key);
     }
 };
+
+
 
 reWrite stats();
 
