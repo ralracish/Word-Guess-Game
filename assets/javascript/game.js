430f2616a2425a19adcbad3d7480eb281432404f
ralracish <carlarblackman@gmail.com>
2018-12-23 12:31:06 -0500

    after tutor file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,88 @@
 //Javascript Document
-
-//Guess the word one letter at a time
-
+// alert("Hello " + "name");
 //Ask the person's name and say hello
-document.getElementById=("hello").innerHTML
-    var name = prompt("What's your name?");
-    alert("Hello " + name);
+// var name=prompt("What's your name")
+// alert("Hello " + name);
+// document.getElementById("hello").innerText=name;
 
-//Set array of possible coffeeshop words
+
+// //Start the game
+
+document.onkeypress = function() {
+    startGame()
+}
+
+// //Set array of possible coffeeshop words
 var coffeeWords = ["coffee", "macchiato", "americano", "mocha", "beans", "cream", "sugar", "cappucino", "latte", "barista", "crema", "espresso", "filter", "redeye"]
+// var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+// var guessedLetters = []; //array for letters guessed
+// var youWin = 0
+// var youLose = 0
+// var guessesLeft = 7
 
-//Pick a random word from the array
-var choice = Math.floor(Math.random()*coffeeWords.length;
+function startGame() {
+// //Pick a random word from the array
+var choice = Math.floor(Math.random()*coffeeWords.length);
 var answer = coffeeWords[choice];
 
-//Put the number of dashes on the screen for the word
-var answerArray = [];
- for (var i = 0; i < coffeeWords.length; i++) 
- answerArray[i] = "_";
+// //Put the number of dashes on the screen for the word
 
-//Ask the player for a letter. Give 5 chances
-document.getElementById="#currentword"
-document.onkeyup = function(event) {
-    var keyPressed = event.key.toLowerCase ();
-var letters = []
-for(var i = 0; i < 5; i++)
-    letters.push(prompt("Guess the word! Type your letter!"));
-    if (letters = [i]) {
-        yesLetter = "Good job! Guess another letter!";
-        //Enter on dashes
-    } else {
-        noLetter = "No. Pick another letter!"
-        //put below Letters Already Guessed
-        document.getElementById="#lettersguesssed"
-    }
-
-//Ask for another letter telling player how many more guesses they have
-var remainingLetters = word.length;
-while (remainingLetters > 0) {
-    (answerArray.join(" "));
-    && var guess.toLowerCase=10-i;
-    alert("Guess the word! Type your letter. You have (10-i) guesses!")
+var answerArray = "";
+ for(var i = 0; i < answer.length; i++){ 
+ answerArray = answerArray + "_";
+ console.log(answerArray)
+ }
+ document.getElementById("currentWord").innerHTML=answerArray;
 }
-//If player wins or loses, play a sound and automatically start another word
-if win document.getElementById="#wins" ()
 
-
-
-
-
-
-
-
-
-
-
-document.onkeyup = function(event) {
+// //Ask the player for a letter. Give 7 chances
+KeyboardEvent.key = function(event) {
     var keyPressed = event.key.toLowerCase ();
-    if(keyPressed) === "t" || keyPressed === "f") 
-    {
-        
-for (i=0;i<10;i++){
-    guess = prompt("Guess the word! Type your letter. You have (10-i) guesses.");
-    if coffeeWords === guess){
-        alert("You guessed the word!");
-        break;
-    }else { i=0
-        guess=prompt("You lost.");
-    }
-    }
-}
-for (var j = 0; j < word.length; j++) {
-     if (word[j] === guess) {
-     answerArray[j] = guess;
-     remainingLetters--;
-     }
 
-    console.log("key pressed: " + event.key);
-    }
-};
+// //Array for guessed letters
+// 
+// var guessedLetters = []
+// for(var i = 0; i < 7; i++){
+//     guessedLetters.push(prompt("Guess the word! Type your letter!"));
+// document.getElementById("guessedLetters").innerText;
+}  
+//     if (guessedLetters = [i]) {
+//         alert("Good job! Guess another letter!");
+//     } 
+//     else {
+//         alert ("No. Try again! Guess the word! Type your letter. You have " + (7-[i]) + "guesses!")
+//     document.getElementById=("remainingGuesses").innerHTML;
+//     }
+// }
+
+//     //Game win
+// if (choice = true) {
+//     alert("#youwin-image")
+// }
+//     //Game lose
+// else {
+//     alert("#gameover-image")
+// }
+
+
+// //If player wins or loses, play a sound and automatically start another word
+// resetGame();
+// updateDisplay();
+
+// //Track Total Wins
+// // if (win document.getElementById="totalwins"
+// // Total Wins:
+// // youWin = youWin + 1
+
+// //Track Total Losses
+// // if lose document.getElementById="totalLosses"
+// // Total Losses:
+// // youLose = youLose + 1
 
 
 
-reWrite stats();
 
-</script>
+
+// reWrite stats();
+
+// </script>
