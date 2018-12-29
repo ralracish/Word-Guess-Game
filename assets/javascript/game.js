11081857f23e3b501b061b8b8f70655c2c6e6196
ralracish <carlarblackman@gmail.com>
2018-12-29 17:36:37 -0500

    removed extra files, working dashes and words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,68 +8,81 @@
     var word = ["mocha", "beans", "cream", "sugar", "crema", "filter", "cup", "tea", "splenda","roast", "body", "blend","drip","flavor"];
 // Choose word randomly
     var choice = Math.floor(Math.random() * word.length);
-    var answer = word[choice]
+    var answer = word[choice];
 //Other variables
     var rightGuess = [];
     var wrongGuess = [];
     var underScore = [];
+    // var youWin = 0;
+    // var youLose = 0;
+    // var remainingGuesses = 10;
+
 
 //DOM manipulation
 var docUnderScore = document.getElementsByClassName("underScore");
 var docrightGuess = document.getElementsByClassName("rightGuess");
 var docwrongGuess = document.getElementsByClassName("wrongGuess");
+var docyouWin = document.getElementsByClassName("wins");
+var docyouLose = document.getElementsByClassName("losses");
 
 //Testing
 console.log(answer);
 
 //Create underscores based on length of word
     var generateUnderscore = () => {
-        for(var i = 0; i < answer.length; i++) {
+        for(var i = 0; i < answer.length; i++){
             underScore.push(' _ ');
         }
         return underScore;
     }
-console.log(generateUnderscore());
+// console.log(generateUnderscore());
 
 //Take input for user guess
-    // document.addEventListener('keypress', (event) => {
-    //     var keyword = String.fromCharCode(event.keyCode);
+    document.addEventListener('keypress', (event) => {
+        var keyword = String.fromCharCode(event.keyCode);
         // console.log(true);
     // )
 // Get user guess
-    // if(answer.indexOf(keyword) > -1) {
+    if(answer.indexOf(keyword) > -1) {
         // console.log(true);
         //add to right words array
-        // rightGuess.push(keyword);
+        rightGuess.push(keyword);
         //replace underscore with right letter
-        // underScore[answer.indexOf(keyword)] = keyword;
-        // docUnderScore[0].innerHTML = underScore.join('');
-        // docrightGuess[0].innerHTML = rightGuess.join(''); 
+        underScore[answer.indexOf(keyword)] = keyword;
+        docUnderScore[0].innerHTML = underScore.join('');
+        docrightGuess[0].innerHTML = rightGuess.join(''); 
         //check to see if user word matches guesses
-    //     if(underScore.join('') == answer) {
-    //         alert("You Win!");
-    //     }
-    // }
-    //     else 
-    //     {
-    //     wrongGuess.push(keyword);
-    //     docwrongGuess[0].innerHTML=wrongGuess;
-    //     }
-    // })
+        if(underScore.join('') == answer) {
+            alert("You Win!");
+            // youWin = youWin++;
+            // docyouWin[0].innerHTML = youWin.join('');
+            // reset();
+        }
+    }
+        else 
+        {
+        wrongGuess.push(keyword);
+        docwrongGuess[0].innerHTML=wrongGuess;
+        // if 
+            // wrongGuess > 9 
+            // {
+            // youLose = youLose++''
+            // docyouLose[0].innerHTML = youLose.join('');
+            // reset();
+        // }
+        }
+    })
 
-    // docrightGuess[0].innerHTML = generateUnderscore().join('');
+    docrightGuess[0].innerHTML = generateUnderscore().join('');
+    
+    
 
 //Check to see if user guess is right
 
 
 // guessedLetters = []; //array for letters guessed
 // var startBtn = document.getElementById('startBtn');
-// var youWin = 0;
-// var youLose = 0;
-// var remainingGuesses = 7;
-// var answer;
-// var answerArray;
-// var newAnswer;
+
 
 // //Game win, track wins, and restart new word
 //     if (answer = true) && (j < 7){        
--- assets/javascript/start.js
+++
@@ -1,9 +0,0 @@
-//Press any letter key to get started..
- document.onkeyup = function(evt) {
-
-   evt = evt || window.event;
-   var charCode = evt.which || evt.keyCode;
-   var charStr = String.fromCharCode(charCode);
-
-   /////Begin when letter key are pressed/////////
-   if (/[a-z]/i.test(charStr)) {
